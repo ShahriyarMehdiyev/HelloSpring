1af6f474f49ade23a71ce1ada4c2087c1dac7dcd
ShahriyarMehdiyev <Shahriyar Mehdiyev@Shurik_M>
2017-09-24 23:26:58 +0400

    Commit 2

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -8,6 +8,7 @@ public class MainApp {
 	private static ApplicationContext context;
 
 	public static void main(String[] args) {
+		
 		  context = new ClassPathXmlApplicationContext("Beans.xml");
 	      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 	      obj.getMessage();
